4e2f1899e248c14840d3a38c7fefa2efdc1c8235
Fábio Bezerra <fabio.bezerra@ufra.edu.br>
2024-11-09 10:32:15 -0300

    Refatoração do padrão Fachada

--- src/main/java/padrao/estrutural/fachada/Cliente.java
+++ src/main/java/padrao/estrutural/fachada/Cliente.java
@@ -7,7 +7,7 @@ public class Cliente {
     public static void main(String[] args) {
         final int MESA = 10;
         Garcom fulano = new Garcom("Fulano");
-        fulano.receberPedido("Quero um Filé da casa e uma garafa de vinho", MESA);
+        fulano.receberPedido("Quero um Filé da casa e uma garrafa de vinho", MESA);
         System.out.println("\n . . . depois de consumir o pedido . . . \n");
         fulano.cobrarPedido(MESA);        
     }
